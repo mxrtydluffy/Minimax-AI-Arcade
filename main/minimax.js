d57b85a9a1cab540a0a50960f58d28f86f16359a
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-10-14 11:54:29 -0700

    [UPDATE] Minimax

--- main/minimax.js
+++ main/minimax.js
@@ -66,3 +66,51 @@ Board.prototype.score = function(depth=0){
         return null;
     }
 }
+
+// AI next move
+Board.prototype.nextMove = function(){
+    let highScore = -Infinity;
+    let address;
+    for(let i = 0; i < this.board.length; i++){
+        for(let j = 0; j< this.board[i].length;j++){
+            if(this.openSquare(this.board[i][j])){
+                this.board[i][j] = this.player;
+                let score = this.minimax(false, 0)
+                this.board[i][j] = '_'
+                if(score > highScore){
+                    highScore = score;
+                    address = {row: i, column: j, score: score}
+                }
+            }
+        }
+    }
+    return address;
+}
+
+Board.prototype.minimax = function(isMaximizing, depth=0){
+    let score = this.score(depth)
+    if(score !== null){
+        return score;
+    }
+
+    let highScore = isMaximizing ? - Infinity : Infinity;
+    let currentPlayer = isMaximizing ? this.player : this.opponent;
+
+    for(let i = 0; i < this.board.length; i++){
+        for(let j = 0; j < this.board[i].length; j++){
+            if(this.openSquare(this.board[i][j])){
+                this.board[i][j] = currentPlayer
+                let score = this. minimax(!isMaximizing, depth + 1)
+                this.board[i][j] = '_'
+                if(isMaximizing){
+                    highScore = Math.max(score, highScore)
+                } else {
+                    highScore = Math.min(score, highScore)
+                }
+            }
+        }
+    }
+    return highScore;
+}
+
+export default Board;
